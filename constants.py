6a387c34cff443b9c6250ddf6a6a29af0846ba4b
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-11-20 15:38:31 +0100

    Update constants.py

--- constants.py
+++ constants.py
@@ -6,6 +6,7 @@ OUTPUT_height = 550
 
 IMAGE = "images/logo.png"
 IMAGE_2 ="images/menu.jpg"
+LINK = "https://www.elskenecologie.nl/contact-elsken-ecologie-nh-terschelling/"
 
 COLORS  =['red', 'blue', 'green', 'purple', 'orange', 'darkred',
          'lightred', 'beige', 'darkblue', 'darkgreen', 'cadetblue',
