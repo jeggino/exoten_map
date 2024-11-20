d9be1e9b2d8f82b10439b9f033cf08a08cdd0933
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-11-20 12:38:38 +0100

    Update login.py

--- functions/login.py
+++ functions/login.py
@@ -9,18 +9,29 @@ df_users = conn.read(ttl=ttl_df_users ,worksheet="df_users")
 
 
 def logIn(df_users):
-    name = st.text_input("Vul uw gebruikersnaam in, alstublieft",value=None)  
-    password = st.text_input("Vul uw wachtwoord in, alstublieft")
-    try:
-        if name == None:
-            st.stop()
-        
-        index = df_users[df_users['username']==name].index[0]
-        true_password = df_users.loc[index,"password"]
-
-    except:
-        st.warning("De gebruikersnaam is niet correct.")
+    option_user = st.selectbox("Selecteer of u een gast of een gebruikersaccount bent. Bedankt.",("Gast", "Gebruiker"),index = None)
+    
+    if option_user == None:
         st.stop()
+
+    elif option_user == "Gebruiker": 
+        name = st.text_input("Vul uw gebruikersnaam in, alstublieft",value=None)  
+        password = st.text_input("Vul uw wachtwoord in, alstublieft")
+        
+        try:
+            if name == None:
+                st.stop()
+            
+            index = df_users[df_users['username']==name].index[0]
+            true_password = df_users.loc[index,"password"]
+    
+        except:
+            st.warning("De gebruikersnaam is niet correct.")
+            st.stop()
+            
+    elif option_user == "Gast":
+        name = None
+        password = None
                              
     if st.button("logIn",use_container_width=True):
         if password == true_password:
@@ -30,8 +41,3 @@ def logIn(df_users):
         else:
             st.markdown(f"Sorry {name.split()[0]}, het wachtwoord is niet correct.")
 
-        
-def logOut():
-    if st.button("logOut",use_container_width=True):
-        del st.session_state.login
-        st.rerun()
