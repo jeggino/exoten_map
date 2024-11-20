869861baca783caf8c5a2544a544010fe53d6742
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-11-20 11:08:27 +0100

    Update ✍️_Voeg_een_waarneming_in.py

--- page/✍️_Voeg_een_waarneming_in.py
+++ page/✍️_Voeg_een_waarneming_in.py
@@ -5,24 +5,13 @@ from streamlit_gsheets import GSheetsConnection
 
 from constants import *
 from functions.inputs import map,input_data
+from css import *
 
 
 
-st.markdown("""
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
-    </style>
-    """,
-    unsafe_allow_html=True)
-
-
-
-reduce_header_height_style = """
-<style>
-    div.block-container {padding-top: 1rem; padding-bottom: 0rem; padding-left: 1rem; padding-right: 1rem; margin-top: 1rem; margin-bottom: 0rem;}
-</style>
-""" 
-
+#---LAYOUT---
+st.markdown(collapsedControl,unsafe_allow_html=True,)
+st.markdown(header_hidden,unsafe_allow_html=True)
 st.markdown(reduce_header_height_style, unsafe_allow_html=True)
 
 
@@ -34,7 +23,7 @@ try:
     conn = st.connection("gsheets", type=GSheetsConnection)
     df_old = conn.read(ttl=ttl_df_points,worksheet="df_observations")
     
-    output_map = map()
+    output_map = map(OUTPUT_width,OUTPUT_height)
     
     try:
         if len(output_map["features"]) >= 1:
@@ -47,4 +36,4 @@ try:
         st.stop()
     
 except:
-    st.switch_page("pages/🧭_navigatie.py")
+    st.switch_page("page/🧭_navigatie.py")
