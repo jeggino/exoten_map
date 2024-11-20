a36ede87f2a71ca2788351f04312ef456cf48c2d
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-11-20 10:07:52 +0100

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,5 +1,16 @@
 ttl_df_points = '30m'
 ttl_df_users = '30m'
 
+OUTPUT_width = '95%'
+OUTPUT_height = 550
+
+IMAGE = "images/logo.png"
+IMAGE_2 ="images/menu.jpg"
+
+COLORS  =['red', 'blue', 'green', 'purple', 'orange', 'darkred',
+         'lightred', 'beige', 'darkblue', 'darkgreen', 'cadetblue',
+         'darkpurple', 'white', 'pink', 'lightblue', 'lightgreen',
+         'gray', 'black', 'lightgray']
+
 SPECIES_LIST = ['Duizendknoop spec','Japanse duizendknoop','Watercrassula','Reuzenbereklauw',
                 'Cotoneaster','Hemelboom']
