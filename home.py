047a8f653932a49c9d373dc2768f85f2fc10af95
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-11-20 13:40:52 +0100

    Update home.py

--- home.py
+++ home.py
@@ -24,7 +24,7 @@ df_users = conn.read(ttl=ttl_df_users,worksheet="df_users")
 #---APP---
 page_1 = st.Page("page/🧭_navigatie.py", title="Navigatie",icon="🧭" )
 page_2 = st.Page("page/✍️_Voeg_een_waarneming_in.py", title="Voeg een waarneming in",icon="✍️" )
-# page_3 = st.Page("page/↩️_Update_een_locatie.py", title="Update een locatie",icon="↩️" )
+page_3 = st.Page("page/↩️_Update_een_locatie.py", title="Update een locatie",icon="↩️" )
 # page_4 = st.Page("page/📊_ Statistik.py", title="Statistik",icon="📊" )
 
 #---APP---
@@ -36,7 +36,7 @@ if "login" not in st.session_state:
     st.stop()
 
 if st.session_state.login['option_user'] == 'Gebruiker':
-    pg = st.navigation([page_1,page_2])
+    pg = st.navigation([page_1,page_2,page_3])
     pg.run()
 
 elif st.session_state.login['option_user'] == 'Gast':
