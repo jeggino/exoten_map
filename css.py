e0d41a28f26638e8d21bef06f269bc6c7467ba42
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-11-20 11:05:05 +0100

    Create css.py

---
+++ css.py
@@ -0,0 +1,20 @@
+collapsedControl = """
+    <style>
+    [data-testid="collapsedControl"] svg {
+        height: 0rem;
+        width: 0rem;
+    }
+    </style>
+    """
+
+header_hidden = """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
+    </style>
+    """
+
+reduce_header_height_style = """
+<style>
+    div.block-container {padding-top: 1rem; padding-bottom: 0rem; padding-left: 1rem; padding-right: 1rem; margin-top: 0rem; margin-bottom: 0rem;}
+</style>
+""" 
