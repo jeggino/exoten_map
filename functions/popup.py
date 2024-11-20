5444fbe039a4b38b2071a95c918d6c30ddedb909
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-11-20 09:27:15 +0100

    Update popup.py

--- functions/popup.py
+++ functions/popup.py
@@ -1,30 +1,18 @@
 import streamlit as st
-from streamlit_gsheets import GSheetsConnection
+
 import pandas as pd
 import datetime
 from datetime import datetime, timedelta, date
 
-import folium
-from folium.plugins import Draw, Fullscreen, LocateControl, GroupedLayerControl
-from streamlit_folium import st_folium
-
 from credentials import *
-import ast
 
-def popup_polygons(row):
+def popup_polygons(row,df_point):
     
     i = row
 
-    project=df_2['project'].iloc[i]
-    gebied=df_2['gebied'].iloc[i]
-    datum=df_2['datum'].iloc[i] 
-    time=df_2['time'].iloc[i]
-    sp = df_2['sp'].iloc[i] 
-    functie=df_2['functie'].iloc[i]
-    opmerking=df_2['opmerking'].iloc[i]
-    aantal=df_2['aantal'].iloc[i]
-    waarnemer=df_2['waarnemer'].iloc[i] 
-       
+    datum=df_point['datum'].iloc[i] 
+    specie = df_point['specie'].iloc[i] 
+    opmerking=df_point['opmerking'].iloc[i]       
 
     left_col_color = "#19a7bd"
     right_col_color = "#f2f0d3"
@@ -34,40 +22,16 @@ def popup_polygons(row):
     <table style="height: 126px; width: 300;">
     <tbody>
     <tr>
-    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Project</span></td>
-    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(project) + """
-    </tr>
-    <tr>
-    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Gebied</span></td>
-    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(gebied) + """
-    </tr>
-    <tr>
     <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Datum</span></td>
     <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(datum) + """
     </tr>
     <tr>
-    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Tijd</span></td>
-    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(time) + """
-    </tr>
-    <tr>
     <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Soort</span></td>
-    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(sp) + """
-    </tr>
-    <tr>
-    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Functie</span></td>
-    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(functie) + """
+    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(specie) + """
     </tr>
     <tr>
     <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Opmerking</span></td>
     <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(opmerking) + """
-    </tr>
-    <tr>
-    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Aantal</span></td>
-    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(int(aantal)) + """
-    </tr>
-    <tr>
-    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Waarnemer</span></td>
-    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(waarnemer) + """
     </tr>
     </tbody>
     </table>
@@ -75,24 +39,13 @@ def popup_polygons(row):
     """
     return html
 
-
-def popup_points(row):
+def popup_points(row,df_point):
     
     i = row
 
-    project=df_2['project'].iloc[i]
-    gebied=df_2['gebied'].iloc[i]
-    datum=df_2['datum'].iloc[i] 
-    time=df_2['time'].iloc[i]
-    verblijf=df_2['verblijf'].iloc[i]
-    sp = df_2['sp'].iloc[i] 
-    functie=df_2['functie'].iloc[i]
-    gedrag=df_2['gedrag'].iloc[i]
-    verblijf=df_2['verblijf'].iloc[i]
-    opmerking=df_2['opmerking'].iloc[i]
-    aantal=df_2['aantal'].iloc[i]
-    waarnemer=df_2['waarnemer'].iloc[i] 
-       
+    datum=df_point['datum'].iloc[i] 
+    specie = df_point['specie'].iloc[i] 
+    opmerking=df_point['opmerking'].iloc[i]       
 
     left_col_color = "#19a7bd"
     right_col_color = "#f2f0d3"
@@ -102,48 +55,16 @@ def popup_points(row):
     <table style="height: 126px; width: 300;">
     <tbody>
     <tr>
-    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Project</span></td>
-    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(project) + """
-    </tr>
-    <tr>
-    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Gebied</span></td>
-    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(gebied) + """
-    </tr>
-    <tr>
     <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Datum</span></td>
     <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(datum) + """
     </tr>
     <tr>
-    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Tijd</span></td>
-    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(time) + """
-    </tr>
-    <tr>
     <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Soort</span></td>
-    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(sp) + """
-    </tr>
-    <tr>
-    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Functie</span></td>
-    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(functie) + """
-    </tr>
-    <tr>
-    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Gedrag</span></td>
-    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(gedrag) + """
-    </tr>
-    <tr>
-    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Verblijf</span></td>
-    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(verblijf) + """
+    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(specie) + """
     </tr>
     <tr>
     <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Opmerking</span></td>
     <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(opmerking) + """
-    </tr>
-    <tr>
-    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Aantal</span></td>
-    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(int(aantal)) + """
-    </tr>
-    <tr>
-    <td style="background-color: """+ left_col_color +""";"><span style="color: #ffffff;">Waarnemer</span></td>
-    <td style="width: 150px;background-color: """+ right_col_color +""";">{}</td>""".format(waarnemer) + """
     </tr>
     </tbody>
     </table>
