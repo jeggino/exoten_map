f052a9f3ff90c76ebc447071bc4a356f737920d2
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-12-08 08:10:46 +0100

    Update home.py

--- home.py
+++ home.py
@@ -5,7 +5,7 @@ import pandas as pd
 conn = st.connection('mysql', type='sql')
 
 
-df = conn.execute('USE ebird; SELECT * from df;', ttl=600)
+df = conn.execute('SELECT * from df;')
 
 df
 # import streamlit as st
